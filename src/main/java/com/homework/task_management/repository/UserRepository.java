513dc2db0594da7249176b1f8a3a9cb3fc93556b
AlexandrLezh <sasha_l@inbox.lv>
2026-07-30 01:42:20 +0300

    User entity and repository added

---
+++ src/main/java/com/homework/task_management/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.homework.task_management.repository;
+
+import com.homework.task_management.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
